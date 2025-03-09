748b0a4d31d1310e1d38d1963369695fb1470c99
rishikeshk2720 <rishikeshk2720@gmail.com>
2025-03-09 12:21:28 +0530

    Removed src folder

--- FoodApp/src/components/Container.jsx
+++
@@ -1,6 +0,0 @@
-import styles from "./container.module.css"
-export default function Container({children}){
-    return (<div className={styles.parentContainer}>
-        {children}
-    </div>)
-}
--- FoodApp/src/components/FoodList.jsx
+++
@@ -1,8 +0,0 @@
-import FoodItem from "./FoodItem"
-export default function FoodList({setFoodId,foodData}){
-    return(<div>
-        {foodData.map((food)=>(
-          <FoodItem key={food.id} setFoodId={setFoodId}food={food}/> 
-        ))}
-    </div>)
-}
